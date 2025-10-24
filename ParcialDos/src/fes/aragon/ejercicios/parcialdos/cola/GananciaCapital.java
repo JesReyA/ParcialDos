a00bd52daa0023e0d0dcdb048c69b78aa505bcf4
JesReyA <jesreyav@gmail.com>
2025-10-23 23:27:33 -0600

    Llenado de cola por medio de CSV terminado

--- ParcialDos/src/fes/aragon/ejercicios/parcialdos/cola/GananciaCapital.java
+++ ParcialDos/src/fes/aragon/ejercicios/parcialdos/cola/GananciaCapital.java
@@ -1,13 +1,13 @@
 package fes.aragon.ejercicios.parcialdos.cola;
 
-import unam.fes.aragon.dinamicas.pilas.Pila;
+import unam.fes.aragon.dinamicas.cola.Cola;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class GananciaCapital {
     public static void main(String[] args) {
-        Pila <Acciones> gananciaPerdida = new Pila<>();
+        Cola<Acciones> gananciaPerdida = new Cola<>();
 
         ManejoArchivos <String> archivoReporte = new ManejoArchivos<>();
         FileReader archivo;
@@ -16,15 +16,19 @@ public class GananciaCapital {
         String [] partes = null;
 
         try{
-            archivo = new FileReader("Acciones.csv");
+            archivo = new FileReader("ParcialDos/Acciones.csv");
             lector= new BufferedReader(archivo);
             while((cadena = lector.readLine()) != null){
                 //Division de la cadena
                 partes = cadena.split(";");
-                Acciones compraVenta = new Acciones('C', 300, 20, 1);
+                Acciones nuevaAccion = new Acciones(partes[0].charAt(0), Integer.parseInt(partes[1]), Integer.parseInt(partes[2]), Integer.parseInt(partes[3]));
+                gananciaPerdida.insertar(nuevaAccion);
             }
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
+
+        Acciones accionActual = gananciaPerdida.elementoSuperior();
+        System.out.println( accionActual.getCantidadAcciones());
     }
 }
